56a36f64b4b4d1f64dc91a6f415a700f84108148
iamnotfelix <andrei.iliescu03@gmail.com>
2025-05-26 01:01:00 +0200

    improve basic graph script

--- src/creation_graphs/create_graph.py
+++ src/creation_graphs/create_graph.py
@@ -7,7 +7,6 @@ import numpy as np
 
 
 def main():
-
     # Read arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('--comments_file', default=None, type=str, required=True, help='Comments file')
@@ -16,62 +15,66 @@ def main():
     parser.add_argument('--threshold', default=1, type=int, required=False, help='Threshold for edge weight')
     args = parser.parse_args()
 
+    # Create target directory if it doesn't exist
+    os.makedirs(args.target_dir, exist_ok=True)
+
     # Read subreddits
+    subreddits = None
     if args.subreddits_file:
         with open(args.subreddits_file, 'r') as f:
             subreddits = set(f.read().strip().split('\n'))
 
-    # Load comments in chunks
-    comments = list()
-    for c in pd.read_json(args.comments_file, compression='bz2', lines=True, dtype=False, chunksize=10000):
-        if args.subreddits_file:
-            c = c[c.subreddit.isin(subreddits)]
-        comments.append(c)
-    comments = pd.concat(comments, sort=True)
-
-    # Do something with comments here
-
+    # Initialize data structures
     authors_counts = {}
     subreddits_to_id = {}
     id_to_subreddits = []
 
-    comments = comments[comments['author'] != '[deleted]']
-    for i, comm in comments.iterrows():
-        if comm['author'] not in authors_counts:
-            authors_counts[comm['author']] = {f'{comm["subreddit"]}': 1}
-        else:
-            if comm['subreddit'] not in authors_counts[comm['author']]:
-                authors_counts[comm['author']][comm['subreddit']] = 1
-            else:
-                authors_counts[comm['author']][comm['subreddit']] += 1 
+    # Process comments in chunks
+    for c in pd.read_json(args.comments_file, compression='bz2', lines=True, dtype=False, chunksize=10000):
+        if subreddits:
+            c = c[c.subreddit.isin(subreddits)]
         
-        if comm['subreddit'] not in subreddits_to_id:
-            subreddits_to_id[comm['subreddit']] = i
-            id_to_subreddits.append(comm['subreddit'])
+        # Filter out deleted authors
+        c = c[c['author'] != '[deleted]']
+        
+        # Process each comment
+        for _, comm in c.iterrows():
+            if comm['author'] not in authors_counts:
+                authors_counts[comm['author']] = {comm['subreddit']: 1}
+            else:
+                authors_counts[comm['author']][comm['subreddit']] = authors_counts[comm['author']].get(comm['subreddit'], 0) + 1
+            
+            if comm['subreddit'] not in subreddits_to_id:
+                subreddits_to_id[comm['subreddit']] = len(subreddits_to_id)
+                id_to_subreddits.append(comm['subreddit'])
 
     num_subreddits = len(subreddits_to_id)
 
-    threshold = 1
+    # Create adjacency matrix
     adjacency_mat = np.zeros((num_subreddits, num_subreddits), dtype=np.int32)
     for author in authors_counts:
         for i in range(num_subreddits):
             for j in range(i + 1, num_subreddits):
-                if not id_to_subreddits[i] in authors_counts[author] or not id_to_subreddits[j] in authors_counts[author]:
-                    continue
-                if authors_counts[author][id_to_subreddits[i]] >= threshold and authors_counts[author][id_to_subreddits[j]] >= threshold:
+                subreddit_i = id_to_subreddits[i]
+                subreddit_j = id_to_subreddits[j]
+                if (subreddit_i in authors_counts[author] and 
+                    subreddit_j in authors_counts[author] and
+                    authors_counts[author][subreddit_i] >= args.threshold and 
+                    authors_counts[author][subreddit_j] >= args.threshold):
                     adjacency_mat[i][j] += 1
+                    adjacency_mat[j][i] = adjacency_mat[i][j]  # Make matrix symmetric
 
-    # Store extracted comments
-    if args.target_dir:
-        target_file = '{}/graph_{}.txt'.format(
-            args.target_dir, re.findall(r'\d{4}-\d{2}', args.comments_file)[0]
-        )
-        with open(target_file, 'w') as f:
-            f.write('node_1,node_2,weighted')
-            for i in range(num_subreddits):
-                for j in range(i + 1, num_subreddits):
-                    if adjacency_mat[i][j] > 0:
-                        f.write(f'{id_to_subreddits[i]},{id_to_subreddits[j]},{adjacency_mat[i][j]}\n')
+    # Store graph
+    target_file = '{}/graph_{}.txt'.format(
+        args.target_dir, re.findall(r'\d{4}-\d{2}', args.comments_file)[0]
+    )
+
+    with open(target_file, 'w') as f:
+        f.write('node_1,node_2,weight\n')
+        for i in range(num_subreddits):
+            for j in range(i + 1, num_subreddits):
+                if adjacency_mat[i][j] > 0:
+                    f.write(f'{id_to_subreddits[i]},{id_to_subreddits[j]},{adjacency_mat[i][j]}\n')
 
 
 if __name__ == '__main__':
